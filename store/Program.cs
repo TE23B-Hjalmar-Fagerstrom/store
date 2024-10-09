4ce8d42260700b829bfa3153e2b042823c55a371
Hjalmar Fagerström <hjalmar.fagerstrom@elev.ga.ntig.se>
2024-10-09 14:44:11 +0200

    klar + en förbättring

--- store/Program.cs
+++ store/Program.cs
@@ -5,34 +5,92 @@ int milk = 10;
 int candy = 20;
 int readyFood = 45;
 int amount = 0;
+int itemSelected = 0;
+int boughtCoke = 0;
+int boughtMilk = 0;
+int boughtCandy = 0;
+int boughtReadyFood = 0;
 
 Console.WriteLine("Du ska till afären och handla lite När du är i afären hittar du bara fyra saker du vill köppa");
 
-Console.WriteLine($"Du har {money} kr och du kan välja mellan");
-
-Console.WriteLine("1: cola(15kr)  2: mjölk(10kr)  3: en påsse godis(20kr) 4: färdig mat(45kr)");
-Console.WriteLine("skriv 1,2,3,4 för att välja");
-
-string item = Console.ReadLine();
-int itemSelected = 0;
-
-int.TryParse(item, out itemSelected);
-
-if (itemSelected == 1 && money > coke)
+while (itemSelected != 5)
 {
-    Console.WriteLine("hur många vill du köppa");
-    string howMany = Console.ReadLine();
-    int.TryParse(howMany, out amount);
-    while (amount * coke > money)
+    Console.WriteLine($"Du har {money} kr och du kan välja mellan");
+
+    Console.WriteLine("1: cola(15kr)  2: mjölk(10kr)  3: en påsse godis(20kr) 4: färdig mat(45kr) 5: lämna afären");
+    Console.WriteLine("skriv 1,2,3,4 eller 5 för att välja");
+
+    string item = Console.ReadLine();
+
+
+    int.TryParse(item, out itemSelected);
+
+    if (itemSelected == 1 && money >= coke)
     {
-        Console.WriteLine("Du har inte råd välj en annan mängd");
-        string howManyRetry = Console.ReadLine();
-        int.TryParse(howManyRetry, out amount);
+        Console.WriteLine("hur många vill du köppa");
+        string howMany = Console.ReadLine();
+        int.TryParse(howMany, out amount);
+        while (amount * coke > money)
+        {
+            Console.WriteLine("Du har inte råd välj en annan mängd");
+            string howManyRetry = Console.ReadLine();
+            int.TryParse(howManyRetry, out amount);
+        }
+        money -= amount * coke;
+        boughtCoke += amount;
     }
-    
+
+    if (itemSelected == 2 && money >= milk)
+    {
+        Console.WriteLine("hur många vill du köppa");
+        string howMany = Console.ReadLine();
+        int.TryParse(howMany, out amount);
+        while (amount * milk > money)
+        {
+            Console.WriteLine("Du har inte råd välj en annan mängd");
+            string howManyRetry = Console.ReadLine();
+            int.TryParse(howManyRetry, out amount);
+        }
+        money -= amount * milk;
+        boughtMilk += amount;
+    }
+
+    if (itemSelected == 3 && money >= candy)
+    {
+        Console.WriteLine("hur många vill du köppa");
+        string howMany = Console.ReadLine();
+        int.TryParse(howMany, out amount);
+        while (amount * candy > money)
+        {
+            Console.WriteLine("Du har inte råd välj en annan mängd");
+            string howManyRetry = Console.ReadLine();
+            int.TryParse(howManyRetry, out amount);
+        }
+        money -= amount * candy;
+        boughtCandy += amount;
+    }
+
+    if (itemSelected == 4 && money >= readyFood)
+    {
+        Console.WriteLine("hur många vill du köppa");
+        string howMany = Console.ReadLine();
+        int.TryParse(howMany, out amount);
+        while (amount * readyFood > money)
+        {
+            Console.WriteLine("Du har inte råd välj en annan mängd");
+            string howManyRetry = Console.ReadLine();
+            int.TryParse(howManyRetry, out amount);
+        }
+        money -= amount * readyFood;
+        boughtReadyFood += amount;
+
+    }
+
+
 }
-
-
+Console.WriteLine("Du går hem efter dina val och funderar om du värkligen hade valt rätt");
+Console.WriteLine($"Du köpte {boughtCoke} color, {boughtMilk} mjölk kartonger, {boughtCandy} påsar godis och {boughtReadyFood} färdig mat");
+Console.WriteLine("Tryck ENTER för att stänga spelet");
 
 
 
